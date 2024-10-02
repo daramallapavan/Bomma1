831847c4d4af82b3607012a8ba86e209ce8f24f9
daramallapavan <daramallapavan123@gmail.com>
2024-10-02 13:34:44 +0530

    One Commit

---
+++ src/test/java/com/example/Bomma1/Bomma1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bomma1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bomma1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
